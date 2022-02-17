a153592df3a4666a0d0b9394fd384712c35371c9
Engr Saad <raselhasandurjoy@gmail.com>
2022-02-17 16:31:32 +0600

    Update Accounts.php

--- application/controllers/Accounts.php
+++ application/controllers/Accounts.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');......................................................................
+defined('BASEPATH') OR exit('No direct script access allowed');.....................................................................
 
 	class Accounts extends CI_Controller{
 
